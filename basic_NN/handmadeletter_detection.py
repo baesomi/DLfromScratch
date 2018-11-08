45dc3dbbde9445b8bc97dae20df55bea114ddcce
baesomi <smbae0249@gmail.com>
2018-11-08 14:36:12 +0900

    MNIST dataset 이용하여 손글씨 detection

---
+++ basic_NN/handmadeletter_detection.py
@@ -0,0 +1,31 @@
+import sys, os
+sys.path.append(os.pardir) # 부모 dir의 파일을 가져올 수 있또록
+import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten=True, normalize=False)
+'''
+# 각 데이터 형상 출력
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
+'''
+
+img = x_train[0]
+label = t_train[0]
+print(label)
+
+print(img.shape)
+img = img.reshape(28,28)
+print(img.shape)
+
+img_show(img)
+
